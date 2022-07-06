0d56eade53812aedcc0731fb4a54d2fc016e1846
vanchuongkieu <vanchuongkieu@gmail.com>
2022-07-06 23:16:14 +0700

    feat: start menu

--- src/components/TaskbarDesktop.vue
+++ src/components/TaskbarDesktop.vue
@@ -31,7 +31,31 @@
       ref="taskbarStartMenuRef"
       class="taskbar-start-menu"
       :class="{ 'start-menu-active': startMenuActive }"
-    ></div>
+    >
+      <div class="taskbar-start-menu--sidebar">
+        <img src="@/assets/icons/window-start-menu.png" class="sidebar-image" />
+      </div>
+      <div class="taskbar-start-menu--menu">
+        <a href="https://www.facebook.com/chuonqit" target="_blank">
+          <div class="bar">
+            <img src="@/assets/icons/facebook.png" class="bar-image" />
+            <u>F</u>acebook
+          </div>
+        </a>
+        <a href="https://github.com/vanchuongkieu" target="_blank">
+          <div class="bar">
+            <img src="@/assets/icons/github.webp" class="bar-image" />
+            <u>G</u>ithub 2
+          </div>
+        </a>
+        <a href="https://github.com/chuonqit" target="_blank">
+          <div class="bar">
+            <img src="@/assets/icons/github.webp" class="bar-image" />
+            <u>G</u>ithub 1
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,6 +67,7 @@ export default {
   name: "TaskbarDesktop",
   data() {
     return {
+      toggle: false,
       window: {},
       taskbarActive: false,
       time: moment().format("hh:mm A"),
@@ -198,6 +223,55 @@ export default {
     bottom: 4rem;
     z-index: 99999;
     visibility: hidden;
+
+    a {
+      text-decoration: none;
+      font-size: 1rem;
+      color: #000;
+    }
+
+    .bar {
+      display: flex;
+      flex-direction: row;
+      padding: 5px 10px 5px 10px;
+      align-items: center;
+
+      &:hover {
+        background: rgb(0, 0, 118);
+        color: white;
+      }
+
+      &-image {
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+        border-radius: 6px;
+      }
+    }
+
+    .divider {
+      width: 100%;
+      height: 1px;
+      background: rgb(123, 125, 123);
+      border-bottom: white solid 1px;
+    }
+
+    &--menu {
+      flex-grow: 1;
+      margin-top: auto;
+    }
+
+    &--sidebar {
+      width: 25px;
+      background: #7b7d7b;
+      height: 100%;
+      display: flex;
+      align-items: flex-end;
+
+      img {
+        width: 100%;
+      }
+    }
 
     &.start-menu-active {
       visibility: visible;
